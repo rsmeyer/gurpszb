1a6c2a45165d5aec5fcc1d4823d99a72dd4d4c69
Meyer <meyer.linux@gmail.com>
2017-07-21 11:04:17 -0300

    page admin

--- index.php
+++ index.php
@@ -4,17 +4,25 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new Slim();
 
 $app->config('debug', true);
 
+//ROTA INDEX
 $app->get('/', function() {
     
 	$page = new Page();
-
 	$page->setTpl("index");
 
+});
+
+//ROTA ADMIN
+$app->get('/mestre', function() {
+    
+	$page = new PageAdmin();
+	$page->setTpl("index");
 
 });
 
